a9ae4070ef808d30647bbc622a95046070288937
masumali23020 <mdmasum0218@gmail.com>
2024-10-13 17:43:04 +0600

    edditing tasker

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export default function AddTaskModal({onSave}) {
-  const [task, setTask] = useState({
+export default function AddTaskModal({onSave,taskToUpdate,handelCloseModel}) {
+  const [task, setTask] = useState(taskToUpdate || {
     id: crypto.randomUUID(),
     tittle: "",
     description: "",
@@ -9,6 +9,8 @@ export default function AddTaskModal({onSave}) {
     priority: "",
     isFavarite: false,
   });
+
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null))
 
   const handelChnge = (event)=> {
     event.preventDefault()
@@ -30,7 +32,7 @@ export default function AddTaskModal({onSave}) {
       <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0"></div>
       <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          sd sre
+          {isAdd ? " Add New Task" : "Edite Task"}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -94,13 +96,13 @@ export default function AddTaskModal({onSave}) {
         </div>
 
         <div className="mt-16 flex justify-between lg:mt-20">
-          <button className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80">
+          <button className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80" onClick={()=>handelCloseModel()}>
             Close
           </button>
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-            onClick={()=> onSave(task)}
+            onClick={()=> onSave(task, isAdd)}
           >
             Save
           </button>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -4,41 +4,59 @@ import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
-
 export default function TaskBoard() {
-    const defaultTask = {
-        id: crypto.randomUUID(),
-        tittle: "Learn react",
-        description:
-          "I want to learn react such than i can treat ii like my slave and make it do whatever i want to be do ",
-        tags: ["wev", "react", "js"],
-        priority: "High",
-        isFavarite: true
-      };
- 
-  const [tasks, setTask] = useState([defaultTask]);
-  const [showAddTaskModal, setShowAddTaskModal] = useState(false)
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    tittle: "Learn react",
+    description:
+      "I want to learn react such than i can treat ii like my slave and make it do whatever i want to be do ",
+    tags: ["wev", "react", "js"],
+    priority: "High",
+    isFavarite: true,
+  };
 
-  const handelAddTask = (newTask) => {
-    setTask([...tasks, newTask])
-    setShowAddTaskModal(false)
+  const [tasks, setTask] = useState([defaultTask]);
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null)
+  const handelAddTask = (newTask,isAdd) => {
+    if(isAdd) {
+      setTask([...tasks, newTask]);
+    }else{
+      setTask(tasks.map(task => {
+        if(task.id === newTask.id){
+          return newTask
+        }
+        return task
+      }))
+    }
    
+    setShowAddTaskModal(false);
+  };
+
+  const hanedelEditeTask = (task) => {
+    setTaskToUpdate(task)
+    setShowAddTaskModal(true)
 
   }
 
-
+  const handelCloseModel = () => {
+    setShowAddTaskModal(false)
+    setTaskToUpdate(null)
+  }
 
   return (
     <section className="mb-20" id="tasks">
-   {showAddTaskModal && < AddTaskModal onSave={handelAddTask} />}
+      {showAddTaskModal && <AddTaskModal onSave={handelAddTask} taskToUpdate={taskToUpdate} handelCloseModel={handelCloseModel}/>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={()=>setShowAddTaskModal(!showAddTaskModal)} />
-          (<TaskList tasks={tasks} />)
+          <TaskActions
+            onAddClick={() => setShowAddTaskModal(!showAddTaskModal)}
+          />
+          (<TaskList tasks={tasks} onEdite={hanedelEditeTask} />)
         </div>
       </div>
     </section>
