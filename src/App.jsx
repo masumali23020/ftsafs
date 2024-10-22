3d81c73fe02593a8661b7ffe5cfdb7eb60a1834e
masumali23020 <mdmasum0218@gmail.com>
2024-10-22 22:54:06 +0600

    footer compomnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css'
-import Header from './componets/Header'
-import MovieList from './componets/MovieList'
-import Sideber from './componets/Sideber'
+import "./App.css";
+import { Footer } from "./componets/Footer";
+import Header from "./componets/Header";
+import MovieList from "./componets/MovieList";
+import Sideber from "./componets/Sideber";
 
 function App() {
- 
-
   return (
     <>
- <Header />
- <main>
- <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-  <Sideber />
-  <MovieList />
-  </div>
-  </main>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sideber />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
