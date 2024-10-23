fe9625f95651e7a732f626caad59d766fd1f1b2f
masumali23020 <mdmasum0218@gmail.com>
2024-10-23 23:45:10 +0600

    ad to card modal show card

---
+++ src/componets/Errow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Errow = (movie) => {
+  return (
+    <div><p className=' text-red-500'>
+        the movie ${movie.title} has been added to the card alreday!`</p></div>
+  )
+}
+
+export default Errow
