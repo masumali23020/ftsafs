f1295b94e3fa989d0f8a6a36b20b9dab193101e1
masumali23020 <mdmasum0218@gmail.com>
2024-10-24 16:07:07 +0600

    add modal show and hide

--- src/components/projects/SubHeader.jsx
+++ src/components/projects/SubHeader.jsx
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import AddModal from "../AddModal";
 
 const SubHeader = () => {
+  const [showAddModal, setShowAddModal] = useState(false);
+  const handelAdd = () => {
+    setShowAddModal(true);
+  };
+  const handelColse = ()=> {
+    setShowAddModal(false)
+  }
   return (
-    <div className="mb-6 flex items-center justify-between">
-      <h2 className="text-2xl font-bold">Projectify</h2>
-      <div className="flex space-x-2">
-        <button className="flex items-center rounded-md bg-gray-700 px-4 py-2 text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="mr-2"
+    <>
+      {" "}
+      {showAddModal && <AddModal onClose={handelColse} />}
+      <div className="mb-6 flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Projectify</h2>
+
+        <div className="flex space-x-2">
+          <button
+            className="flex items-center rounded-md bg-gray-700 px-4 py-2 text-white"
+            onClick={handelAdd}
           >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
-            <path d="M15 12h-6" />
-            <path d="M12 9v6" />
-          </svg>
-          Add
-        </button>
-      </div>
-    </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="mr-2"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+              <path d="M15 12h-6" />
+              <path d="M12 9v6" />
+            </svg>
+            Add
+          </button>
+        </div>
+      </div>{" "}
+    </>
   );
 };
 
